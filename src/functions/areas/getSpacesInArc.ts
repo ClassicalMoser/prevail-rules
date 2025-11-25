2258ffe4d03f57b66afb4582841f2a69f84d05e9
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-11-25 01:31:52 -0600

    Add arc-finding logic

---
+++ src/functions/areas/getSpacesInArc.ts
@@ -0,0 +1,33 @@
+import type { Board, BoardCoordinate } from "src/entities/board/board.js";
+
+import type { UnitFacing } from "src/entities/unit/unitFacing.js";
+import { filterUndefinedSpaces, getFrontSpaces } from "../index.js";
+
+export function getSpacesInArc(
+  board: Board,
+  coordinate: BoardCoordinate<Board>,
+  facing: UnitFacing,
+  range: number
+): Set<BoardCoordinate<Board>> {
+  // Start with the origin space
+  const spacesInArc = new Set<BoardCoordinate<Board>>([coordinate]);
+  // Add the spaces in front of the origin space
+  for (let i = 0; i < range; i++) {
+    // Iterate forward by spaces in front, up to the range.
+    const currentSpacesInArc = [...spacesInArc];
+    for (const space of currentSpacesInArc) {
+      // Get the spaces in front of the current space
+      const spacesInFront = getFrontSpaces(board, space, facing);
+      // Add the spaces in front of the current space to the set
+      for (const space of spacesInFront) {
+        spacesInArc.add(space);
+      }
+    }
+  }
+  // Remove the origin space
+  spacesInArc.delete(coordinate);
+  // Filter out undefined values
+  const validSpacesInArc = filterUndefinedSpaces(spacesInArc);
+  // Return the set of valid spaces in the arc
+  return validSpacesInArc;
+}
